5e223af57f8d9c8c170e92d672c8752ad2345c12
Aique Montes Maestre <UO261072@uniovi.es>
2020-03-04 15:13:54 +0100

    Añadir rutas prototipo vista

--- src/containers/addRoute/addRoute.container.js
+++ src/containers/addRoute/addRoute.container.js
@@ -1,41 +1,11 @@
-import React from 'react';
-import readJSON from './parser';
-
+import addRouteForm from './addRouteForm';
 
 
 const addRoute= ()=>{
-    let name='';
-    /*var fs = require('fs');
-    fs.readFile('./prueba.json',(err,data)=>{
-        if(err) throw err;
-        let prueba=JSON.parse(data);
-        console.log(prueba.name);
-    });*/
-
-
-    let ruta="Ruta";
-    const handleSubmit=(event)=>{
-        //let json="{'name':'"+event.name+"'}";
-        alert('A name was submitted: '+name);
-    };
     
-    const handleChange=(event)=> {
-        name=event.target.value;
-    }
-
-    
+    let a=new addRouteForm();
     return(
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Introduce el nombre:</label>
-                <input type="text" id="name" value={name} onChange={handleChange}/>
-            </div>
-            <div>
-                <label>Intoduce la duracion de la ruta:</label>
-                <input type="text" />
-            </div>
-            <input type="submit" action="Submit"/>
-        </form>
+        a.render()
     );
 };
 
---
+++ src/containers/addRoute/addRouteForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+export default class addRouteForm extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: "prueba",
+          duration: "2"
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    
+    handleInputChange(event) {
+        const target = event.target;
+        console.log(target.name);
+        if(target.name==="name"){
+            this.state.name=target.value;
+        }
+        if(target.name==="duration"){
+            this.state.duration=target.value;
+        }
+        console.log(this.state);
+        console.log(JSON.parse('{"name":'+ this.state.name +',"duration":'+ this.state.duration +'}'))
+       
+    }
+    
+    handleSubmit(event) {
+        
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>Introduce el nombre:</label>
+                    <input type="text" name="name" checked={this.state.name} onChange={this.handleInputChange}/>
+                </div>
+                <div>
+                    <label>Intoduce la duracion de la ruta:</label>
+                    <input type="text" name="duration" checked={this.state.duration} onChange={this.handleInputChange}/>
+                </div>
+                <input type="button" onClick={this.handleSubmit}/>
+            </form>
+        );
+    }
+}
