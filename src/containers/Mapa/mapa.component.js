c0a57c36cd17ac3250aeee5e0b744cdf52dd475d
pelayo-rodriguez-garcia <pelayo.r.g@gmail.com>
2020-03-04 20:41:47 +0100

    Ejemplo de ruta básica

--- src/containers/Mapa/mapa.component.js
+++ src/containers/Mapa/mapa.component.js
@@ -1,44 +1,50 @@
 /*jshint esversion: 6 */
 import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
-
-
-
-
-
-
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, Polyline } from 'react-google-maps';
 
 const Mapa = () => {
 
 
     const markersArray = [];
-    
-    async function addMarker(latLng) {
+    const pathCoordinates = [
+        { lat: -34.397, lng: 150.644 },
+        { lat: -35, lng: 150 }
+    ];
+
+    var WrappedMap = withScriptjs(withGoogleMap(props => <GoogleMap
+        defaultZoom={7}
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        onClick={handleClick}
+    >
+        <Marker position={{ lat: -34.397, lng: 150.644 }}></Marker>
+        <Marker position={{ lat: -35, lng: 150 }}></Marker>
+        <Polyline
+            path={pathCoordinates}
+            geodesic={true}
+            options={{
+                strokeColor: "#ff2527",
+                strokeOpacity: 0.75,
+                strokeWeight: 2,
+            }}
+        />
+
+
+    </GoogleMap>));
+
+
+    function handleClick(e) {
+        addMarker(e.latLng);
+
+    }
+    function addMarker(latLng) {
         let marker = <Marker
             position={{ lat: latLng.lat(), lng: latLng.lng() }}
         >
 
         </Marker>
         markersArray.push(marker);
-
-
     }
 
-    var WrappedMap = withScriptjs(withGoogleMap(props => <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-        onClick={handleClick}
-    >
-
-
-
-
-    </GoogleMap>));
-
-    async function handleClick(e) {
-        addMarker(e.latLng);
-
-    }
 
     return (<div style={{ width: "100vw", hegiht: "100vh" }}> <WrappedMap
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
