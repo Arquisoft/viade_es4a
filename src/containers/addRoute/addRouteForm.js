5893d1d8a1773c92becceb959eced95db4654f6b
Aique Montes Maestre <UO261072@uniovi.es>
2020-03-24 16:10:38 +0100

    Añadido Parseador a la creacion de rutas para procesar todos los datos

--- src/containers/addRoute/addRouteForm.js
+++ src/containers/addRoute/addRouteForm.js
@@ -1,24 +1,26 @@
 import React, {useState} from 'react';
 import auth from "solid-auth-client";
 import FC from 'solid-file-client';
+import Parser from "./Parser";
 
 import {RouteAddDiv,RouteAddTitle} from './addRoute.style';
 
 const addRouteForm=(props)=>{
     const [name,setName]=useState();
-    const [duration,setDuration]=useState();
+    const [description,setDescription]=useState();
 
 
 
     const onSubmit= async (e)=>{
         console.log(name);
-        const file='{"name":'+name+',"duration":'+duration+'}'
+        //const file='{"name":'+name+',"description":'+description+'}'
+        var file=Parser(name,description,null);
         console.log(new Date());
         const {webId} = props;
         console.log(webId);
         //const fileName=name.replace(/\s+/g,'');
 
-        const url=webId.split("profile/card#me")[0]+"public/rutas/"+new Date()+".json";
+        const url=webId.split("profile/card#me")[0]+"public/"+new Date()+".json";
         const fc   = new FC( auth );
         await fc.createFile(url, file, "application/geo+json", {});
         console.log("subido");
@@ -34,10 +36,10 @@ const addRouteForm=(props)=>{
                     <input type="text" name="name"  value={name} onChange={e=>setName(e.target.value)}/>
                 </div>
                 <div>
-                    <label>Intoduce la duracion de la ruta:</label>
-                    <input type="text" name="duration"  value={duration} onChange={e=>setDuration(e.target.value)}/>
+                    <label>Intoduce la descripcion de la ruta:</label>
+                    <input type="text" name="description"  value={description} onChange={e=>setDescription(e.target.value)}/>
                 </div>
-                <input type="submit" value="Añadir" onChange={onSubmit}/>
+                <input type="button" value="Añadir" onClick={onSubmit}/>
             </form>
         </RouteAddDiv>
 
