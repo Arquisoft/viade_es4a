28f39abbca7d3b8c0f63505e99ac273cadcbc1e0
Aique Montes Maestre <UO261072@uniovi.es>
2020-03-25 15:55:53 +0100

    Mejorado upload route para que siempre suba json

--- src/containers/UploadRoute/upload-route.container.js
+++ src/containers/UploadRoute/upload-route.container.js
@@ -2,6 +2,7 @@ import React from "react";
 import auth from "solid-auth-client";
 import FC from 'solid-file-client';
 import {RouteAddDiv} from '../addRoute/addRoute.style';
+import Parser from "../addRoute/Parser";
 
 const UploadRoute = (props) => {
 
@@ -12,9 +13,11 @@ const UploadRoute = (props) => {
         fileReader.onload = async (event) => {
             const fc   = new FC( auth );
             const nombre=event.target.fileName;
-            const url=webId.split("profile/card#me")[0]+"public/rutas/"+nombre;
+            var json = JSON.parse(event.target.result);
+            json=Parser(json.name,json.description);
+            const url=webId.split("profile/card#me")[0]+"public/"+nombre;
             console.log(url);
-            await fc.createFile(url, event.target.result, "application/geo+json", {});
+            await fc.createFile(url, json, "application/geo+json", {});
             console.log("subido");
         };
         fileReader.readAsText(e.target.files[0]);
